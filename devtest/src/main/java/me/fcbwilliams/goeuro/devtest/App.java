89477830b6b19253d71cefa1f4c45b8f7ac8b677
Francis Williams <fcbwilliams@gmail.com>
2015-05-29 11:46:31 +0100

    Initial Commit. Maven Project.

---
+++ devtest/src/main/java/me/fcbwilliams/goeuro/devtest/App.java
@@ -0,0 +1,13 @@
+package me.fcbwilliams.goeuro.devtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
