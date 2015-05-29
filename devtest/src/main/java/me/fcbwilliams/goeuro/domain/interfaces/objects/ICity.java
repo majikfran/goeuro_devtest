7d606b8c74322cc1c29a858261c792714ed3f814
Francis Williams <fcbwilliams@gmail.com>
2015-05-29 16:24:21 +0100

    Added ICity Interface to domain.interfaces

---
+++ devtest/src/main/java/me/fcbwilliams/goeuro/domain/interfaces/objects/ICity.java
@@ -0,0 +1,9 @@
+package me.fcbwilliams.goeuro.domain.interfaces.objects;
+
+public interface ICity {
+	int get_id();
+	String getName();
+	String getType();
+	double getLatitude();
+	double getLongitude();
+}
