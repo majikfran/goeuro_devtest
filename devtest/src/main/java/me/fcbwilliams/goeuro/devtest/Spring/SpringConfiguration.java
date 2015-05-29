c6d4eda562ac1df9ea315e041245733288d13262
Francis Williams <fcbwilliams@gmail.com>
2015-05-29 20:20:49 +0100

    Updated to inject properties from application.properties

---
+++ devtest/src/main/java/me/fcbwilliams/goeuro/devtest/Spring/SpringConfiguration.java
@@ -0,0 +1,30 @@
+package me.fcbwilliams.goeuro.devtest.Spring;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import me.fcbwilliams.goeuro.domain.interfaces.objects.*;
+import me.fcbwilliams.goeuro.repositories.goeuro.rest.PositionEndpoint;
+import me.fcbwilliams.goeuro.repositories.interfaces.IEndpoint;
+
+
+@Configuration
+@ComponentScan("me.fcbwilliams.goeuro")
+@PropertySource("classpath:application.properties")
+public class SpringConfiguration {
+
+	@Value("${goeuro.apiBasePath}")
+	private String apiBasePath;
+	
+	@Value("${goeuro.apiVersionPath}")
+	private String apiVersionPath;
+	
+	@Bean
+	public IEndpoint<ICity> cityRepository()
+	{
+		return new PositionEndpoint(apiBasePath, apiVersionPath);
+	}
+}
