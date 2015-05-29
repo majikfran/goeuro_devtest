8aee7ca8b5700c9e7ae53c895389c08a9218b4bb
Francis Williams <fcbwilliams@gmail.com>
2015-05-29 20:22:13 +0100

    Updated to load properties from application.properties. AND to configure jacksonMessageConverter bean for json conversions

---
+++ devtest/src/test/java/me/fcbwilliams/goeuro/devtest/Spring/TestSpringConfiguration.java
@@ -0,0 +1,50 @@
+package me.fcbwilliams.goeuro.devtest.Spring;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import me.fcbwilliams.goeuro.domain.interfaces.objects.ICity;
+import me.fcbwilliams.goeuro.repositories.goeuro.rest.PositionEndpoint;
+import me.fcbwilliams.goeuro.repositories.interfaces.IEndpoint;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class TestSpringConfiguration {
+	
+	@Value("${goeuro.apiBasePath}")
+	private String apiBasePath;
+	
+	@Value("${goeuro.apiVersionPath}")
+	private String apiVersionPath;
+	
+	@Bean
+	public IEndpoint<ICity> cityRepository()
+	{
+		return new PositionEndpoint(apiBasePath, apiVersionPath);
+	}
+	
+	@Bean
+	public MappingJackson2HttpMessageConverter jacksonMessageConverter()
+	{
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		List<MediaType> mediaTypes = new ArrayList<MediaType>();
+		mediaTypes.add(new MediaType("application", "json", Charset.forName("UTF-8")));
+		converter.setSupportedMediaTypes(mediaTypes);
+		
+		return converter;
+	}
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+}
