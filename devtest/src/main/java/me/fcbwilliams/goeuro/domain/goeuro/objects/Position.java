c468b47e201d16996e807e83f098ad504c5b67e5
Francis Williams <fcbwilliams@gmail.com>
2015-05-30 17:27:47 +0100

    Added setters

--- devtest/src/main/java/me/fcbwilliams/goeuro/domain/goeuro/objects/Position.java
+++ devtest/src/main/java/me/fcbwilliams/goeuro/domain/goeuro/objects/Position.java
@@ -18,9 +18,19 @@ public class Position implements IPosition {
 		return _id;
 	}
 	
+	public void set_id(int _id)
+	{
+		this._id = _id;
+	}
+	
 	public String getName()
 	{
 		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
 	}
 	
 	public String getType()
@@ -28,13 +38,28 @@ public class Position implements IPosition {
 		return type;
 	}
 	
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+
 	public double getLatitude()
 	{
 		return latitude;
 	}
 	
+	public void setLatitude(double latitude)
+	{
+		this.latitude = latitude;
+	}
+	
 	public double getLongitude()
 	{
 		return longitude;
+	}
+	
+	public void setLongitude(double longitude)
+	{
+		this.longitude = longitude;
 	}
 }
