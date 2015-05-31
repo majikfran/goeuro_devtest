dc1a1ddf12d0589a935aea6f6866e97323a67938
Francis Williams <fcbwilliams@gmail.com>
2015-05-31 23:33:48 +0100

    Bootstrapped spring with the commandline arguments to allow injection

--- devtest/src/main/java/me/fcbwilliams/goeuro/devtest/App.java
+++ devtest/src/main/java/me/fcbwilliams/goeuro/devtest/App.java
@@ -1,5 +1,11 @@
 package me.fcbwilliams.goeuro.devtest;
 
+import me.fcbwilliams.goeuro.devtest.Spring.SpringConfiguration;
+import me.fcbwilliams.goeuro.devtest.UI.interfaces.IUserInterface;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+
 
 /**
  * Hello world!
@@ -10,6 +16,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SimpleCommandLinePropertySource clps = new SimpleCommandLinePropertySource(args);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.getEnvironment().getPropertySources().addFirst(clps);
+        ctx.register(SpringConfiguration.class);
+        ctx.refresh();
+
+        ctx.getBean(IUserInterface.class).start();
+        ctx.close();
     }
 }
