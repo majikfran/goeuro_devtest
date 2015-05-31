ec607bf109ed596f0e54ff63b1ee1f01b3003410
Francis Williams <fcbwilliams@gmail.com>
2015-05-31 18:40:33 +0100

    Changed retention policy to RUNTIME

--- devtest/src/main/java/me/fcbwilliams/goeuro/devtest/annotations/ModelInfo.java
+++ devtest/src/main/java/me/fcbwilliams/goeuro/devtest/annotations/ModelInfo.java
@@ -1,5 +1,9 @@
 package me.fcbwilliams.goeuro.devtest.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ModelInfo {
 	public String name();
 
