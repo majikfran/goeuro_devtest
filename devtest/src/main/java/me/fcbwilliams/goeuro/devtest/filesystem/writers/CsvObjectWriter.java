0bbe317069fa71161cd9bcc3ab9b875e212a6dfb
Francis Williams <fcbwilliams@gmail.com>
2015-05-31 19:26:43 +0100

    Updated CsvObjectWriter to reflect change to opencsv version

--- devtest/src/main/java/me/fcbwilliams/goeuro/devtest/filesystem/writers/CsvObjectWriter.java
+++ devtest/src/main/java/me/fcbwilliams/goeuro/devtest/filesystem/writers/CsvObjectWriter.java
@@ -11,7 +11,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import au.com.bytecode.opencsv.CSVWriter;
+import com.opencsv.CSVWriter;
+
 import me.fcbwilliams.goeuro.devtest.annotations.ModelInfo;
 import me.fcbwilliams.goeuro.devtest.filesystem.interfaces.IFileObjectWriter;
 import me.fcbwilliams.goeuro.devtest.util.interfaces.IObjectConverter;
@@ -23,8 +24,9 @@ public class CsvObjectWriter<T> implements IFileObjectWriter<T>{
 	
 	@Override
 	public void writeData(T[] objects, String path) throws IOException {
-		CSVWriter csvWriter = new CSVWriter(new FileWriter(path, false), ',');
+		CSVWriter csvWriter = new CSVWriter(new FileWriter(path, false), CSVWriter.DEFAULT_SEPARATOR);
 		
+		List<String[]> toWrite = new ArrayList<String[]>();
 		List<String> headers = new ArrayList<String>();
 		
 		for(Field f : ((T) objects[0]).getClass().getDeclaredFields())
@@ -34,13 +36,15 @@ public class CsvObjectWriter<T> implements IFileObjectWriter<T>{
 				headers.add(modelInfos[0].name());
 		}
 		
-		csvWriter.writeNext(headers.toArray(new String[headers.size()]));
-		
+		toWrite.add(headers.toArray(new String[headers.size()]));
+	
 		for(T o : objects)
 		{
 			String[] data = tToStringArrayConverter.convert(o);
-			csvWriter.writeNext(data);
+			toWrite.add(data);
 		}
+		
+		csvWriter.writeAll(toWrite, false);
 		
 		csvWriter.close();
 	}
