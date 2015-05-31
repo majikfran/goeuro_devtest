cd01bd603f2acff93a52408107f57e904049ec4d
Francis Williams <fcbwilliams@gmail.com>
2015-05-31 18:18:49 +0100

    Implementation of converter for position objects to string arrays

---
+++ devtest/src/main/java/me/fcbwilliams/goeuro/devtest/util/converters/PositionToStringArrayConverter.java
@@ -0,0 +1,25 @@
+package me.fcbwilliams.goeuro.devtest.util.converters;
+
+import org.springframework.stereotype.Component;
+
+import me.fcbwilliams.goeuro.devtest.util.interfaces.IObjectConverter;
+import me.fcbwilliams.goeuro.domain.goeuro.objects.Position;
+import me.fcbwilliams.goeuro.domain.interfaces.objects.IPosition;
+
+@Component
+public class PositionToStringArrayConverter implements IObjectConverter<IPosition, String[]> {
+
+	@Override
+	public String[] convert(IPosition object) {
+		String[] strings = new String[5];
+		
+		strings[0] = String.valueOf(object.get_id());
+		strings[1] = object.getName();
+		strings[2] = object.getType();
+		strings[3] = String.valueOf(object.getLongitude());
+		strings[4] = String.valueOf(object.getLatitude());
+		
+		return strings;
+	}
+
+}
