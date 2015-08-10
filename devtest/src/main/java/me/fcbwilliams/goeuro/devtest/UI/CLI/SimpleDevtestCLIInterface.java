8ccbd186875a8c8398a4edf2eda0fbc94c83c8a5
Francis Williams <fcbwilliams@gmail.com>
2015-08-10 18:37:59 +0200

    Fixed header file resolution

--- devtest/src/main/java/me/fcbwilliams/goeuro/devtest/UI/CLI/SimpleDevtestCLIInterface.java
+++ devtest/src/main/java/me/fcbwilliams/goeuro/devtest/UI/CLI/SimpleDevtestCLIInterface.java
@@ -1,6 +1,11 @@
 package me.fcbwilliams.goeuro.devtest.UI.CLI;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import me.fcbwilliams.goeuro.devtest.UI.interfaces.IUserInterface;
@@ -10,6 +15,11 @@ import me.fcbwilliams.goeuro.endpoints.interfaces.IRestEndpoint;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * A basic CLI user interface.
+ * 
+ * @author Francis
+ */
 public class SimpleDevtestCLIInterface implements IUserInterface {
 
 	@Autowired
@@ -18,11 +28,23 @@ public class SimpleDevtestCLIInterface implements IUserInterface {
 	IFileObjectWriter<ILocation> writer;
 	
 	private String parameter;
-	private String outputPath;
+	private String headerPath;
+	private String greeting;
+	private String farewell;
 	
-	public SimpleDevtestCLIInterface(String parameter, String outputPath) {
+	/**
+	 * Constructor
+	 * 
+	 * @param parameter The parameter for the REST service request
+	 * @param headerPath The path to the desired CLI Header
+	 * @param greeting A greeting message
+	 * @param farewell A farewell message
+	 */
+	public SimpleDevtestCLIInterface(String parameter, String headerPath, String greeting, String farewell) {
 		this.parameter = parameter;
-		this.outputPath = outputPath;
+		this.headerPath = headerPath;
+		this.greeting = greeting;
+		this.farewell = farewell;
 	}	
 	
 	@Override
@@ -30,6 +52,7 @@ public class SimpleDevtestCLIInterface implements IUserInterface {
 		printHeader();
 		printGreeting();
 		
+		//Request data from the endpoint.
 		List<? extends ILocation> positions = (List<? extends ILocation>)endpoint.getData(parameter);
 		try {
 			writer.writeData(positions);
@@ -47,18 +70,38 @@ public class SimpleDevtestCLIInterface implements IUserInterface {
 		printFarewell();
 	}
 	
+	/**
+	 * Prints the header message from file.
+	 */
 	private void printHeader()
 	{
-		
+		try{
+			URI headerAbsolutePath = this.getClass().getResource(headerPath).toURI();
+			
+			List<String> headerLines = Files.readAllLines(Paths.get(headerAbsolutePath), Charset.forName("UTF-8"));
+			
+			for(String s : headerLines)
+			{
+				System.out.println(s);
+			}
+		}
+		catch(IOException e)
+		{
+			System.out.println("Unable to find header file");
+		}
+		catch(URISyntaxException e)
+		{
+			System.out.println("header URI Syntax Exception\nUnable to print header");
+		}
 	}
 	
 	private void printGreeting()
 	{
-		
+		System.out.println(greeting);
 	}
 	
 	private void printFarewell()
 	{
-		
+		System.out.println(farewell);
 	}
 }
